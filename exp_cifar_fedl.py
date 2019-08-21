963e4bed184443a7350fcc64c79fb67000cc830e
eliott15 <47332569+eliott15@users.noreply.github.com>
2019-08-21 15:52:05 +0200

    Add files via upload

---
+++ exp_cifar_fedl.py
@@ -0,0 +1,262 @@
+import sys
+import syft as sy
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import time
+import torch.utils.model_zoo as model_zoo
+import math
+
+num_workers = int(sys.argv[-1])
+__all__ = [
+    'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
+    'vgg19_bn', 'vgg19',
+]
+
+
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+}
+
+
+class VGG(nn.Module):
+
+    def __init__(self, features, num_classes=10, init_weights=True):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.shape[0], -1)
+        x = self.classifier(x)
+        x = F.log_softmax(x,dim=1)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+def vgg11(pretrained=False, **kwargs):
+    """VGG 11-layer model (configuration "A")
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['A']), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg11']))
+    return model
+
+
+
+def vgg11_bn(pretrained=False, **kwargs):
+    """VGG 11-layer model (configuration "A") with batch normalization
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfg['A'], batch_norm=True), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg11_bn']))
+    return model
+
+
+
+class Arguments():
+    def __init__(self, no_cuda):
+        self.batch_size = 64
+        self.test_batch_size = 1000
+        self.epochs = 10
+        self.lr = 0.001
+        self.momentum = 0.5
+        self.no_cuda = no_cuda
+        self.seed = 1
+        self.log_interval = 30
+        self.save_model = False
+
+
+
+def model_size(model):
+    size = 0
+    for p in model.parameters():
+        size += p.element_size()*p.nelement()
+    return size
+
+def train(args, model, device, federated_train_loader, optimizer, epoch, clients_mem):
+    model.train()
+    for batch_idx, (data, target) in enumerate(federated_train_loader): # <-- now it is a distributed dataset
+        start = time.time()
+        model.send(data.location) # <-- NEW: send the model to the right location
+        end = time.time()
+        print("send",end-start)
+        data, target = data.to(device), target.to(device)
+        i = int(data.location.id.split()[-1])
+
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+        start = time.time()
+        model.get() # <-- NEW: get the model back
+
+        end = time.time()
+        print("get",end-start)
+
+        clients_mem[i] += 2*model_size(model)
+
+        if batch_idx % args.log_interval == 0:
+            loss = loss.get() # <-- NEW: get the loss back
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * args.batch_size, len(federated_train_loader) * args.batch_size,
+                100. * batch_idx / len(federated_train_loader), loss.item()))
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+def experiment(num_workers,no_cuda):
+
+    # Creating num_workers clients
+    clients = []
+    hook = sy.TorchHook(torch)
+    clients_mem = torch.zeros(num_workers)
+    for i in range(num_workers):
+        clients.append(sy.VirtualWorker(hook, id="c "+str(i)))
+
+
+    # Initializing arguments, with GPU usage or not
+    args = Arguments(no_cuda)
+
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    if use_cuda:
+    # TODO Quickhack. Actually need to fix the problem moving the model to CUDA\n",
+        torch.set_default_tensor_type(torch.cuda.FloatTensor)
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+    kwargs = {'num_workers': 0, 'pin_memory': False} if use_cuda else {}
+
+    # Federated data loader
+    transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    federated_train_loader = sy.FederatedDataLoader( # <-- this is now a FederatedDataLoader
+      datasets.CIFAR10('../data', train=True, download=True,
+                     transform=transform)
+      .federate(clients), # <-- NEW: we distribute the dataset across all the workers, it's now a FederatedDataset
+      batch_size=args.batch_size, shuffle=True, **kwargs)
+
+    test_loader = torch.utils.data.DataLoader(
+      datasets.CIFAR10('../data', train=False, transform=transform),
+      batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+
+    #creating the models for each client
+    #models,optimizers = [], []
+    #print(device)
+    #for i in range(num_workers):
+        #print(i)
+    #    models.append(Net1().to(device))
+    #    models[i] = models[i].send(clients[i])
+    #    optimizers.append(optim.SGD(params=models[i].parameters(),lr=0.1))
+
+
+
+    start = time.time()
+    #%%time
+    model = vgg11().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr) # TODO momentum is not supported at the moment
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, federated_train_loader, optimizer, epoch, clients_mem)
+        test(args, model, device, test_loader)
+
+    if (args.save_model):
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
+    end = time.time()
+    print(end - start)
+    print("Memory exchanged : ",clients_mem)
+    return clients_mem
+
+
+experiment(num_workers,False)
